238d6bc488333deddf6f47b47ef2a556ed357a87
Afonso <67978137+AfonsoPaula@users.noreply.github.com>
2023-12-28 16:40:21 +0000

    add funcoes.h - organization

---
+++ Project/6version/funcoes.h
@@ -0,0 +1,11 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+void createDatabase(char* dbname);
+void showDatabases();
+void dropDatabase(char* dbname);
+void useDatabase(char* dbname);
+void whichdb();
+void createTable(char* tableName, int numColumns);
+
+#endif // FUNCOES_H
